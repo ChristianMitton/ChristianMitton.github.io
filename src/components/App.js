a5b68252073111938f4b62290f5f26fb5aa5b79d
ChristianMitton <christianmitton@gmail.com>
2020-01-15 14:53:42 -0500

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import logo from '../logo.svg';
+import '../styles/style.css';
+import PathFinder from './PathFinder';
+
+function App() {
+  return (
+    <div className="App">      
+      <PathFinder />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PathFinder.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class PathFinder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                TESTING!
+                {/* <Node /> */}
+            </div>
+        )
+    }
+
+}
+
+export default PathFinder;
