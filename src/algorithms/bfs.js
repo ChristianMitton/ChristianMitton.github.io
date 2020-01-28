305729b3f38e840b7ae2c2cbd372af90d721a763
ChristianMitton <christianmitton@gmail.com>
2020-01-27 22:37:36 -0500

    included bfs animation

--- src/algorithms/bfs.js
+++ src/algorithms/bfs.js
@@ -9,43 +9,74 @@ let Queue = require('../dataStructures/Queue');
 
 // graph.printGrid();
 
-// let t1 = graph.grid[2][2];
-// let t2 = graph.grid[4][4];
+// let t1 = grid[2][2];
+// let t2 = grid[4][4];
 //let t2 = new GraphNode("<node>", 5,100);
+function clone(obj) {
+    if (null == obj || "object" != typeof obj) return obj;
+    var copy = new obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+    }
+    return copy;
+}
 
-function bfs(graph, startNode, endNode){    
+function copy2dArrayOfObjects(array){
+    let result = []
+    for(let row in array){
+        let newRow = []
+        for(let col in array[row]){
+            let copiedObj = clone(array[row][col]);
+            newRow.push(copiedObj);
+        }
+        result.push(newRow);
+    }
+
+    return result;
+}
+
+function bfs(mainGrid, startNode, endNode, numRows, numCols){    
     console.log(`Start Node: row:${startNode.row} col:${startNode.col}`);
     console.log(`End Node: row:${endNode.row} col:${endNode.col}`);
     console.log();
     let queue = new Queue();
 
-    graph.grid[startNode.row][startNode.col].visited = true;
+    //console.log(`Here is the main grid after splicing: ${mainGrid}`);
+    //let grid = [...mainGrid];
+    let grid = copy2dArrayOfObjects(mainGrid);
+    //console.log(`Here is the temp grid after splicing: ${grid}`);
+
+    let visitedNodes = [];    
+    
+    grid[startNode.row][startNode.col].visited = true;
+
     queue.enqueue(startNode);   
 
     while(!queue.isEmpty()){
-        let currentNode = queue.dequeue();
-        //TODO: do something visually with current node here     
+        let currentNode = queue.dequeue();        
+        // append current node to visited array
+        visitedNodes.push(currentNode);
+
         let {row, col} = currentNode;        
 
-        console.log(`currentNode: <node>(${row},${col})`);
+        //console.log(`currentNode: <node>(${row},${col})`);
 
-        if(row === endNode.row && col === endNode.col){
-            //TODO: do something visually with final node here
+        if(row === endNode.row && col === endNode.col){            
             console.log("Reached destination");
-            return;
+            return visitedNodes;
         }
         
-        addChildrenToQueue(graph, currentNode, queue); 
+        addChildrenToQueue(grid, currentNode, queue, numRows, numCols); 
 
         //process.stdout.write("Queue contents after adding children:");
-        queue.printGraphQueue();
+        //queue.printGraphQueue();
     }
 
     console.log("Did not reach destination");
-    return false;
+    return visitedNodes;
 }
 
-function addChildrenToQueue(graph, currentNode, queue){
+function addChildrenToQueue(grid, currentNode, queue, numRows, numCols){    
 
     let {row, col} = currentNode;    
 
@@ -57,56 +88,56 @@ function addChildrenToQueue(graph, currentNode, queue){
     //! top left corner
     if(row === 0 && col === 0) {            
         //right child
-        if(!graph.grid[row][col+1].visited){
-            graph.grid[row][col+1].visited = true;
-            queue.enqueue(graph.grid[row][col+1])
+        if(!grid[row][col+1].visited){
+            grid[row][col+1].visited = true;
+            queue.enqueue(grid[row][col+1])
         }
         //bottom child
-        if(!graph.grid[row+1][col].visited){
-            graph.grid[row+1][col].visited = true;
-            queue.enqueue(graph.grid[row+1][col])    
+        if(!grid[row+1][col].visited){
+            grid[row+1][col].visited = true;
+            queue.enqueue(grid[row+1][col])    
         }
         return;             
     }
     //! top right corner
-    else if(row === 0 && col === (graph.numCol-1)){
+    else if(row === 0 && col === (numCols-1)){
         //left child
-        if(!graph.grid[row][col-1].visited){
-            graph.grid[row][col-1].visited = true;
-            queue.enqueue(graph.grid[row][col-1]) 
+        if(!grid[row][col-1].visited){
+            grid[row][col-1].visited = true;
+            queue.enqueue(grid[row][col-1]) 
         }
         //bottom child
-        if(!graph.grid[row+1][col].visited){
-            graph.grid[row+1][col].visited = true;
-            queue.enqueue(graph.grid[row+1][col]) 
+        if(!grid[row+1][col].visited){
+            grid[row+1][col].visited = true;
+            queue.enqueue(grid[row+1][col]) 
         }
         return;
     }
     //! bottom right corner
-    else if(row === (graph.numRow-1) && col === (graph.numCol-1)){
+    else if(row === (numRows-1) && col === (numCols-1)){
         //left child
-        if(!graph.grid[row][col-1].visited){
-            graph.grid[row][col-1].visited = true;
-            queue.enqueue(graph.grid[row][col-1]) 
+        if(!grid[row][col-1].visited){
+            grid[row][col-1].visited = true;
+            queue.enqueue(grid[row][col-1]) 
         }
         //top child
-        if(!graph.grid[row-1][col].visited){
-            graph.grid[row-1][col].visited = true;
-            queue.enqueue(graph.grid[row-1][col]) 
+        if(!grid[row-1][col].visited){
+            grid[row-1][col].visited = true;
+            queue.enqueue(grid[row-1][col]) 
         }
         return;
     }
     //! bottom left corner
-    else if(row === (graph.numRow-1) && col === 0){
+    else if(row === (numRows-1) && col === 0){
         //right child
-        if(!graph.grid[row][col+1].visited){
-            graph.grid[row][col+1].visited = true;
-            queue.enqueue(graph.grid[row][col+1]) 
+        if(!grid[row][col+1].visited){
+            grid[row][col+1].visited = true;
+            queue.enqueue(grid[row][col+1]) 
         }
         //top child
-        if(!graph.grid[row-1][col].visited){
-            graph.grid[row-1][col].visited = true;
-            queue.enqueue(graph.grid[row-1][col]) 
+        if(!grid[row-1][col].visited){
+            grid[row-1][col].visited = true;
+            queue.enqueue(grid[row-1][col]) 
         }
         return;
     }
@@ -118,76 +149,76 @@ function addChildrenToQueue(graph, currentNode, queue){
     //! top border
     else if(row === 0){
         //left child  
-        if(!graph.grid[row][col-1].visited){
-            graph.grid[row][col-1].visited = true;
-            queue.enqueue(graph.grid[row][col-1])            
+        if(!grid[row][col-1].visited){
+            grid[row][col-1].visited = true;
+            queue.enqueue(grid[row][col-1])            
         }
         //right child
-        if(!graph.grid[row][col+1].visited){
-            graph.grid[row][col+1].visited = true;
-            queue.enqueue(graph.grid[row][col+1]) 
+        if(!grid[row][col+1].visited){
+            grid[row][col+1].visited = true;
+            queue.enqueue(grid[row][col+1]) 
         }
         //bottom child
-        if(!graph.grid[row+1][col].visited){
-            graph.grid[row+1][col].visited = true;
-            queue.enqueue(graph.grid[row+1][col]) 
+        if(!grid[row+1][col].visited){
+            grid[row+1][col].visited = true;
+            queue.enqueue(grid[row+1][col]) 
         }
         return;
     }
     //! right border
-    else if(col === graph.numCol-1){
+    else if(col === numCols-1){
         //left child
-        if(!graph.grid[row][col-1].visited){
-            graph.grid[row][col-1].visited = true;
-            queue.enqueue(graph.grid[row][col-1])            
+        if(!grid[row][col-1].visited){
+            grid[row][col-1].visited = true;
+            queue.enqueue(grid[row][col-1])            
         }
         //top child
-        if(!graph.grid[row-1][col].visited){
-            graph.grid[row-1][col].visited = true;
-            queue.enqueue(graph.grid[row-1][col]) 
+        if(!grid[row-1][col].visited){
+            grid[row-1][col].visited = true;
+            queue.enqueue(grid[row-1][col]) 
         }
         //bottom child
-        if(!graph.grid[row+1][col].visited){
-            graph.grid[row+1][col].visited = true;
-            queue.enqueue(graph.grid[row+1][col]) 
+        if(!grid[row+1][col].visited){
+            grid[row+1][col].visited = true;
+            queue.enqueue(grid[row+1][col]) 
         }
         return;
     }
     //! bottom border
-    else if(row === graph.numRow-1){
+    else if(row === numRows-1){
         //left child
-        if(!graph.grid[row][col-1].visited){
-            graph.grid[row][col-1].visited = true;
-            queue.enqueue(graph.grid[row][col-1])            
+        if(!grid[row][col-1].visited){
+            grid[row][col-1].visited = true;
+            queue.enqueue(grid[row][col-1])            
         }
         //top child
-        if(!graph.grid[row-1][col].visited){
-            graph.grid[row-1][col].visited = true;
-            queue.enqueue(graph.grid[row-1][col]) 
+        if(!grid[row-1][col].visited){
+            grid[row-1][col].visited = true;
+            queue.enqueue(grid[row-1][col]) 
         }
         //right child
-        if(!graph.grid[row][col+1].visited){
-            graph.grid[row][col+1].visited = true;
-            queue.enqueue(graph.grid[row][col+1]) 
+        if(!grid[row][col+1].visited){
+            grid[row][col+1].visited = true;
+            queue.enqueue(grid[row][col+1]) 
         }
         return;
     }
     //! left border
     else if(col === 0){
         //top child
-        if(!graph.grid[row-1][col].visited){
-            graph.grid[row-1][col].visited = true;
-            queue.enqueue(graph.grid[row-1][col]) 
+        if(!grid[row-1][col].visited){
+            grid[row-1][col].visited = true;
+            queue.enqueue(grid[row-1][col]) 
         }
         //right child
-        if(!graph.grid[row][col+1].visited){
-            graph.grid[row][col+1].visited = true;
-            queue.enqueue(graph.grid[row][col+1]) 
+        if(!grid[row][col+1].visited){
+            grid[row][col+1].visited = true;
+            queue.enqueue(grid[row][col+1]) 
         } 
         //bottom child
-        if(!graph.grid[row+1][col].visited){
-            graph.grid[row+1][col].visited = true;
-            queue.enqueue(graph.grid[row+1][col]) 
+        if(!grid[row+1][col].visited){
+            grid[row+1][col].visited = true;
+            queue.enqueue(grid[row+1][col]) 
         } 
         return;
     } 
@@ -198,24 +229,24 @@ function addChildrenToQueue(graph, currentNode, queue){
     */
     else {
         //top child
-        if(!graph.grid[row-1][col].visited){
-            graph.grid[row-1][col].visited = true;
-            queue.enqueue(graph.grid[row-1][col]) 
+        if(!grid[row-1][col].visited){
+            grid[row-1][col].visited = true;
+            queue.enqueue(grid[row-1][col]) 
         }
         //right child
-        if(!graph.grid[row][col+1].visited){
-            graph.grid[row][col+1].visited = true;
-            queue.enqueue(graph.grid[row][col+1]) 
+        if(!grid[row][col+1].visited){
+            grid[row][col+1].visited = true;
+            queue.enqueue(grid[row][col+1]) 
         } 
         //bottom child
-        if(!graph.grid[row+1][col].visited){
-            graph.grid[row+1][col].visited = true;
-            queue.enqueue(graph.grid[row+1][col]) 
+        if(!grid[row+1][col].visited){
+            grid[row+1][col].visited = true;
+            queue.enqueue(grid[row+1][col]) 
         } 
         //left child
-        if(!graph.grid[row][col-1].visited){
-            graph.grid[row][col-1].visited = true;
-            queue.enqueue(graph.grid[row][col-1])            
+        if(!grid[row][col-1].visited){
+            grid[row][col-1].visited = true;
+            queue.enqueue(grid[row][col-1])            
         }
     }   
 
