38eee08801e39f7e2429a83700b8272d7959344f
ChristianMitton <christianmitton@gmail.com>
2020-02-12 22:39:10 -0500

    added ability to move start and end nodes

--- src/components/PathFinder.js
+++ src/components/PathFinder.js
@@ -12,22 +12,27 @@ let aStar = require('../algorithms/aStar')
 let copyObjects = require('../algorithms/copyObjects')
 //let { copy2dArrayOfObjects } = require('../algorithms/bfs');
 
-const START_NODE_ROW_TEST = 10;
-const START_NODE_COL_TEST = 21;
-const FINISH_NODE_ROW_TEST = 10//5//14//14//8//7;
-const FINISH_NODE_COL_TEST = 37//37//32//18+5+3;
+let START_NODE_ROW_TEST = 10;
+let START_NODE_COL_TEST = 18;
+let FINISH_NODE_ROW_TEST = 10//5//14//14//8//7;
+let FINISH_NODE_COL_TEST = 49-18//37//32//18+5+3;
 
 const numRows = 20;
 const numCols = 50;
 
 let start = true;
+//let clickedStart = false;
+//let clickedEnd = false;
 
 class PathFinder extends Component {
     constructor(props) {
         super(props);
         this.state = {
             grid: [],
-            mouseIsPressed: false
+            mouseIsPressed: false,
+            clickedStart: false,
+            clickedFinish: false
+
         };
     }
 
@@ -42,6 +47,29 @@ class PathFinder extends Component {
 
     //when mouse button is pressed
     handleMouseDown(row, col){
+        //if the start node is clicked
+        if(row === START_NODE_ROW_TEST && col === START_NODE_COL_TEST){
+            console.log("clicked Start node")
+
+            const newGrid = this.getNewGridWithUpdatedStartNode(this.state.grid, row, col, START_NODE_ROW_TEST, START_NODE_COL_TEST)        
+            this.setState({
+                grid: newGrid,
+                clickedStart: true
+            })
+            return
+        }
+
+        if(row === FINISH_NODE_ROW_TEST && col === FINISH_NODE_COL_TEST){
+            console.log("clicked End node")
+
+            const newGrid = this.getNewGridWithUpdatedFinishNode(this.state.grid, row, col, FINISH_NODE_ROW_TEST, FINISH_NODE_COL_TEST)        
+            this.setState({
+                grid: newGrid,
+                clickedFinish: true
+            })
+            return
+        }
+        
         const newGrid = this.getNewGridWithWallToggled(this.state.grid, row, col);
         this.setState({
             grid: newGrid,
@@ -51,7 +79,24 @@ class PathFinder extends Component {
     }
 //////////////////////////////////////////
     //when mouse is hovering
-    handleMouseEnter(row, col){
+    handleMouseEnter(row, col){        
+        
+        //if the start button is clicked
+        // if(row === START_NODE_ROW_TEST && col === START_NODE_COL_TEST){
+        if(this.state.clickedStart){
+            const newGrid = this.getNewGridWithUpdatedStartNode(this.state.grid, row, col, START_NODE_ROW_TEST, START_NODE_COL_TEST)
+            this.setState({
+                grid: newGrid,                
+            })
+            return
+        }
+        if(this.state.clickedFinish){
+            const newGrid = this.getNewGridWithUpdatedFinishNode(this.state.grid, row, col, FINISH_NODE_ROW_TEST, FINISH_NODE_COL_TEST)
+            this.setState({
+                grid: newGrid,                
+            })
+            return
+        }
         //if the mouse isnt pressed, do nothing
         if(!this.state.mouseIsPressed){
             console.log(`hovering over node ${row},${col}`)
@@ -71,7 +116,9 @@ class PathFinder extends Component {
         //once you release the mouse button, set mouseIsPressed to false
         console.log(`release mouse button on node (${row},${col})`)
         this.setState({
-            mouseIsPressed: false
+            mouseIsPressed: false,
+            clickedStart: false,
+            clickedFinish: false
         })
     }
 
@@ -202,6 +249,48 @@ class PathFinder extends Component {
             newNode.isWall = true
             newGrid[row][col] = newNode
             return newGrid
+    }
+
+    getNewGridWithUpdatedStartNode(grid, row, col, oldRow, oldCol){
+        const newGrid = copyObjects.copy2dArrayOfObjects(grid)        
+        
+        const oldStartNode = newGrid[oldRow][oldCol]
+        const newStartNode = newGrid[row][col]
+
+        const oldStartNodeCopy = copyObjects.clone(oldStartNode)
+        const newStartNodeCopy = copyObjects.clone(newStartNode)
+
+        START_NODE_ROW_TEST = row
+        START_NODE_COL_TEST = col  
+
+        oldStartNodeCopy.isStart = false
+        newStartNodeCopy.isStart = true
+        
+        newGrid[oldRow][oldCol] = oldStartNodeCopy        
+        newGrid[row][col] = newStartNodeCopy
+
+        return newGrid
+    }
+    
+    getNewGridWithUpdatedFinishNode(grid, row, col, oldRow, oldCol){
+        const newGrid = copyObjects.copy2dArrayOfObjects(grid)        
+        
+        const oldFinishNode = newGrid[oldRow][oldCol]
+        const newFinishNode = newGrid[row][col]
+
+        const oldStartNodeCopy = copyObjects.clone(oldFinishNode)
+        const newStartNodeCopy = copyObjects.clone(newFinishNode)
+
+        FINISH_NODE_ROW_TEST = row
+        FINISH_NODE_COL_TEST = col  
+
+        oldStartNodeCopy.isFinish = false
+        newStartNodeCopy.isFinish = true
+        
+        newGrid[oldRow][oldCol] = oldStartNodeCopy        
+        newGrid[row][col] = newStartNodeCopy
+
+        return newGrid
     }
 }
 
